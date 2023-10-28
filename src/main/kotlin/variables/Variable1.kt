877130b2f8375b764e39430ede7254c7430d340d
dev <oumadeni33@gmail.com>
2023-10-28 21:56:40 +0300

    initial commit

---
+++ src/main/kotlin/variables/Variable1.kt
@@ -0,0 +1,17 @@
+package variables
+
+//public class Person {
+//    public static void main(String [] args) {
+//        System.out.println("Hello world");
+//        int a
+//    }
+//
+//}
+
+fun main() {
+    var age = 45
+    age = 34
+    age = 56
+    println("Hello world")
+    println("my age is $age")
+}
